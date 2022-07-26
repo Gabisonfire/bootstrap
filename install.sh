709a7d12343cb8876257d135207c94462260c6bb
Gabisonfire <gab.despres@gmail.com>
2022-07-25 22:16:08 -0400

    Added install script

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@ cd ~/Documents
 pacman -S ansible git
 git clone https://github.com/Gabisonfire/bootstrap.git
 cd bootstrap
-ansible-galaxy collection install -r requirements.yml
+ansible-galaxy collection install -r collections/requirements.yml
 export VAULT_SKIP_VERIFY=true
 export VAULT_ADDR=https://vault.k8s.luna.net
 export VAULT_TOKEN=$(vault login -token-only -method=ldap username=gabisonfire)
