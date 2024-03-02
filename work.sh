4e301e7957c4f8dc311107daf0cff261821507df
Gabisonfire <gab.despres@gmail.com>
2024-03-01 22:12:39 -0500

    Update for current setup

--- work.sh
+++ work.sh
@@ -6,7 +6,4 @@ pip install hvac --break-system-packages
 git clone https://github.com/Gabisonfire/bootstrap.git
 cd bootstrap
 ansible-galaxy collection install -r collections/requirements.yml
-export VAULT_SKIP_VERIFY=true
-export VAULT_ADDR=https://vault.k8s.luna.net
-export VAULT_TOKEN=$(vault login -token-only -method=ldap username=gabisonfire)
 ansible-playbook -i inventory work.yml
